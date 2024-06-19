194faa9c3b826699faf659a79da5750a1077ed77
颜玉刚 <yanyugang@gmail.com>
2024-06-19 13:57:47 +0800

    long text inference

--- api.py
+++ api.py
@@ -1,28 +1,27 @@
 import io
-
+from io import BytesIO
+import time
 import os
 import wave
+import json
 import numpy as np
 import torch
-from fastapi import FastAPI, Query, Depends
+from fastapi import FastAPI, Query, Depends, Response
 from fastapi.responses import StreamingResponse
 
 from pydantic import BaseModel
-from LangSegment import LangSegment
 import ChatTTS
 import uvicorn
 
 import argparse
 from loguru import logger
-from utils.zh_normalization import text_normalize
-from utils.text_split_method import text_split_registry
-
+from utils import split_text, combine_audio, pack_audio, batch_split
 
 class TTS(BaseModel):
     """TTS GET/POST request"""
     text: str = Query("欢迎使用ChatTTS API", description="text to synthesize")
-    spk: str = Query("random", description="speaker id")
-    text_split_method: str = Query("cut2", description="text split mode")
+    spk_id: str = Query("default", description="speaker id")
+    media_type: str = Query("wav", description="media type")
 
 
 class Speaker(BaseModel):
@@ -36,66 +35,78 @@ args = None
 
 speaker = {}
 
-curr_speaker = None
-LangSegment.setfilters(["zh", "ja", "en"])
-
-
-def wave_header_chunk(frame_input=b"", channels=1, sample_width=2, sample_rate=24000):
-    wav_buf = io.BytesIO()
-    with wave.open(wav_buf, "wb") as vfout:
-        vfout.setnchannels(channels)
-        vfout.setsampwidth(sample_width)
-        vfout.setframerate(sample_rate)
-        vfout.writeframes(frame_input)
-    wav_buf.seek(0)
-    return wav_buf.read()
-
-
-def infer(texts, spk="random"):
-    global curr_speaker
-    if speaker.get(spk) is None or spk == "random":
+def generate_tts_audio(text, spk_id = "default", max_length = 80):
+    if spk_id is None or spk_id == "random" or speaker.get(spk_id) is None:
+        print("load spk random")
         spk_emb = chat.sample_random_speaker()
-        curr_speaker = spk_emb
+        
+        speed = 3
+        oral = 0
+        laugh = 0
+        bk = 4
+        top_P = 0.7
+        top_K = 20
+        temperature = 0.3
     else:
-        spk_emb = speaker[spk]
-    params_infer_code = {'spk_emb': spk_emb, }
-    yield wave_header_chunk()
-    for text in texts:
-        audio_data = chat.infer(text, use_decoder=True,
-                                params_infer_code=params_infer_code,
-                                do_text_normalization=False)[0]
-        audio_data = audio_data / np.max(np.abs(audio_data))
-        chunks = (audio_data * 32768).astype(np.int16)
-        for chunk in chunks:
-            if chunk is not None:
-                chunk = chunk.tobytes()
-                yield chunk
+        print(f"load spk {spk_id}")
+        spk_emb = speaker.get(spk_id)['emb']
+        spk_data = speaker.get(spk_id)
 
+        speed = max(spk_data.get("speed", 3), 0)
+        oral = spk_data.get("oral", 0)
+        laugh = spk_data.get("laugh", 0)
+        bk = spk_data.get("break", 4)
+        top_P = spk_data.get("top_P", 0.7)
+        top_K = spk_data.get("top_K", 20)
+        temperature = spk_data.get("temperature", 0.3)
+
+    if oral < 0 or oral > 9 or laugh < 0 or laugh > 2 or bk < 0 or bk > 7:
+        raise ValueError("oral_(0-9), laugh_(0-2), break_(0-7) out of range")
+
+    params_infer_code = {
+        'spk_emb': spk_emb,
+        'prompt': f'[speed_{speed}]',
+        'top_P': top_P,
+        'top_K': top_K,
+        'temperature': temperature,
+    }
+
+    refine_text_prompt = f"[oral_{oral}][laugh_{laugh}][break_{bk}]"
+    params_refine_text = {
+        'prompt': refine_text_prompt,
+        'top_P': top_P,
+        'top_K': top_K,
+        'temperature': temperature,
+    }
+
+    start_time = time.time()
+    all_wavs = []
+    texts = split_text(text, max_length)
+
+    batch_size = 4
+    for batch in batch_split(texts, batch_size):
+        wavs = chat.infer(batch, params_infer_code=params_infer_code, params_refine_text=params_refine_text, use_decoder=True, skip_refine_text=False)
+        all_wavs.extend(wavs)
+
+        torch.cuda.empty_cache()
+
+    audio = combine_audio(all_wavs)
+    audio = (audio * 32768).astype(np.int16).squeeze()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Saving audio for spk_id = {spk_id} , took {elapsed_time:.2f}s")
+
+    return audio
 
 def tts_handle(params: TTS):
     logger.debug(params)
-    # 主要是为了格式化一下数字和一些文字的读法
 
-    # 将长文本分割成短文本,最好就用cut2
-    texts = text_split_registry[params.text_split_method](params.text)
-    text_list = []
-    for text in texts:
-        for tmp in LangSegment.getTexts(text):
-            normalize = text_normalize(tmp.get("text"))
-            logger.debug(f"{tmp} {normalize}")
-            if normalize != "" and tmp.get("lang") == "en" and normalize not in ["."]:
-                if len(text_list) > 0:
-                    text_list[-1] += normalize
-                else:
-                    text_list.append(normalize)
-            elif tmp.get("lang") == "zh":
-                text_list.append(normalize)
-            else:
-                text_list.append(tmp.get("text"))
-    wavs = infer(text_list, params.spk)
-    logger.debug(text_list)
-    return StreamingResponse(wavs, media_type="audio/wav")
+    audio_data = generate_tts_audio(params.text, params.spk_id, 120)
+    sr = 24000
+    audio_data = pack_audio(BytesIO(), audio_data, sr, params.media_type).getvalue()
 
+    return Response(audio_data, media_type=f"audio/{params.media_type}")
 
 @app.get("/")
 async def index(params: TTS = Depends(TTS)):
@@ -106,34 +117,30 @@ async def index(params: TTS = Depends(TTS)):
 async def index_post(params: TTS):
     return tts_handle(params)
 
-
-@app.post("/speaker")
-async def speaker_handle(params: Speaker):
-    if params.name != "":
-        # 使用torch将上一个发音人embedding保存到本地
-        torch.save(curr_speaker, os.path.join(args.spk_dir, f"{params.name}.pt"))
-        speaker[params.name] = curr_speaker
-    return {'code': 200, 'msg': 'success'}
-
-
 @app.get("/speaker")
 async def speaker_list():
     l = list(speaker.keys())
     return {'code': 200, 'msg': 'success', 'data': l}
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0")
-    parser.add_argument("--port", type=int, default=12456)
+    parser.add_argument("--port", type=int, default=8002)
     parser.add_argument("--model-dir", type=str, default="./models")
     parser.add_argument("--compile", type=bool, default=False)
-    parser.add_argument("--spk-dir", type=str, default="./configs")
+    parser.add_argument("--spk-dir", type=str, default="./speaker_config")
     args = parser.parse_args()
     logger.debug(f"model_dir: {args.model_dir}")
     for file in os.listdir(args.spk_dir):
-        if file.endswith(".pt"):
-            logger.debug(f"loading speaker model:{file[:-3]}")
-            speaker[file[:-3]] = torch.load(os.path.join(args.spk_dir, file))
+        if file.endswith(".json"):
+            logger.debug(f"loading speaker config: {file[:-5]}")
+            with open(os.path.join(args.spk_dir, file), 'r') as f:
+                config = json.load(f)
+            spk_name = file[:-5]
+            if 'spk_model' in config:
+                speaker[spk_name] = config
+                speaker[spk_name]['emb'] = torch.load(config['spk_model'])
+            else:
+                logger.warning(f"Skipping speaker '{spk_name}' as 'spk_model' field is not found in the configuration.")
     chat.load_models(source="local", force_redownload=False, local_path=args.model_dir, compile=args.compile)
     uvicorn.run(app, host=args.host, port=args.port)
